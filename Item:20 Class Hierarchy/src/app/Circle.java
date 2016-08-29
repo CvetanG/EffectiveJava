3da672d97d9320530761be81d8c69667dffb8e4a
cvetan <cgg@abv.bg>
2016-08-29 14:13:07 +0300

    Add Item 20

---
+++ Item:20 Class Hierarchy/src/app/Circle.java
@@ -0,0 +1,14 @@
+package app;
+
+public class Circle extends Figure {
+	final double radius;
+
+	Circle(double radius) {
+		this.radius = radius;
+	}
+	
+	@Override
+	double area() {
+		return Math.PI * (radius * radius);
+	}
+}
